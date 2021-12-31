77fe6a6e73f47a6f722daa14f864124293542b1c
jr-lol <jr-lol@live.com>
2021-12-31 16:16:35 -0300

    ajuste de highlight de textReferences

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -49,9 +49,14 @@ local function lsp_highlight_document(client)
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
+      hi LspReferenceRead ctermbg=237 guibg=#282c34
+      hi LspReferenceText ctermbg=237 guibg=#282c34
+      hi LspReferenceWrite ctermbg=237 guibg=#282c34
+
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
