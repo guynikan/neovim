a830ef6bf0d9ee4abac77eb8b744be53e7812cea
jr-lol <jr-lol@live.com>
2022-01-07 21:56:57 -0300

    adicionado plugin shade

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -116,6 +116,8 @@ return packer.startup(function(use)
 
   use { 'mateusbraga/vim-spell-pt-br' }
 
+  use { 'sunjon/Shade.nvim' }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
